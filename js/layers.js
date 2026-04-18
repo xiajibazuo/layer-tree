e522ea6dd73fc1c84c4d292d8694f59c9b4b31ac
xiajibazuo <2460125883@qq.com>
2026-04-18 22:17:38 +0800

    更新 layers.js

--- js/layers.js
+++ js/layers.js
@@ -6,7 +6,7 @@ addLayer("L", {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#7F0000",
+    color: "#007F7F",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "层级", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
