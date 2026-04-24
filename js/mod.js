9ab697a3f547a7426e04824b980d3577bb3d2770
xiajibazuo <2460125883@qq.com>
2026-04-24 22:18:17 +0800

    更新 mod.js

--- js/mod.js
+++ js/mod.js
@@ -21,7 +21,7 @@ let changelog = `<h1>Changelog:</h1><br>
 		- Added things.<br>
 		- Added stuff.`
 
-let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
+let winText = `恭喜！您已成功完成游戏并到达终点，不过目前......`
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
 // (The ones here are examples, all official functions are already taken care of)
